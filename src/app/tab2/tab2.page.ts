ae8b78155842386305867789a5e68659738f0c07
hugofsd <hugo619@outlook.com>
2021-10-03 01:56:38 -0300

    Atualização de plugins, service e capacitor

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from 'src/service/api.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,13 +10,54 @@ import { Router } from '@angular/router';
 })
 export class Tab2Page {
 
+  loading: boolean;
+  latitude: number;
+  longitude: number;
+
+  dados: any;
+
   constructor(
     public router: Router,
+    public apiService: ApiService,
   ) {}
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit() {
+
+   this.getLocation();
+  }
+
+
 
 
   sair(){
     this.router.navigate(['/home']);
   }
 
+  async getLocation() {
+    this.loading = true;
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    this.loading = false;
+
+    console.log(this.latitude);
+    console.log(this.latitude);
+
+    this.api();
+  }
+
+  api() {
+  this.apiService.buscarDados(this.latitude, this.longitude)
+  .subscribe(
+    data =>{
+      this.dados = data;
+      console.log(data);
+    }
+  );
+  }
 }
+// function subscribe(arg0: (data: any) => void) {
+//   throw new Error('Function not implemented.');
+// }
+
--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CepServiceService {
+export class ApiService {
 
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  private API = environment.apiurl;
+ // private API = environment.apiurl;
 
   constructor(public http: HttpClient) { }
 
-  poluicaoService(lat: any, lon: any): Observable<any>{
+  buscarDados(lat: any, lon: any): any{
     // eslint-disable-next-line
-    return this.http.get(`${this.API}/v2/nearest_city?lat=` + lat + `&lon=` + lon + `&key=f8be2bb4-74ed-4e4c-b9df-7865a95e00d9`);
+    return this.http.get(`http://api.airvisual.com/v2/nearest_city?lat=` + lat + `&lon=` + lon + `&key=f8be2bb4-74ed-4e4c-b9df-7865a95e00d9`);
   }
 }
