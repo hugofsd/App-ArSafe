3c234cfd43690613827efd0b3c9cdb8fb558f08b
hugofsd <hugo619@outlook.com>
2021-10-05 19:07:25 -0300

    Atualização no Get

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -9,13 +9,10 @@ import { environment } from 'src/environments/environment';
 })
 export class ApiService {
 
-  // eslint-disable-next-line @typescript-eslint/naming-convention
- // private API = environment.apiurl;
-
   constructor(public http: HttpClient) { }
 
   buscarDados(lat: any, lon: any): any{
     // eslint-disable-next-line
-    return this.http.get(`http://api.airvisual.com/v2/nearest_city?lat=` + lat + `&lon=` + lon + `&key=f8be2bb4-74ed-4e4c-b9df-7865a95e00d9`);
+    return this.http.get(`https://api.airvisual.com/v2/nearest_city?lat=` + lat + `&lon=` + lon + `&key=f8be2bb4-74ed-4e4c-b9df-7865a95e00d9`);
   }
 }
