ee9c867aafa66859bef774a93b2b6cbbff1f57c0
hugofsd <hugo619@outlook.com>
2021-10-04 21:40:26 -0300

    Bug de acesso as telas resolvido. Criação da page extra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,29 +4,33 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: ' ',
     pathMatch: 'full'
   },
 
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'extra',
+    loadChildren: () => import('./extra/extra.module').then( m => m.ExtraPageModule)
   },
 
-  {
-    path: 'home',
-    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
-  },
 
-  {
-    path: 'clima',
-    loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
+  // },
 
-  {
-    path: 'ar',
-    loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
-  },
+  // {
+  //   path: 'clima',
+  //   loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
+  // },
+
+  // {
+  //   path: 'ar',
+  //   loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
+  // },
 
   // {
   //   path: 'clima',
