2fa13719a77ada3f5ce5229ba072b0975b9c6f33
hugofsd <hugo619@outlook.com>
2021-10-14 22:09:42 -0300

    Atualização de service

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +10,10 @@ export class ApiService {
 
   buscarDados(lat: any, lon: any): any{
     // eslint-disable-next-line
-    return this.http.get(`https://api.airvisual.com/v2/nearest_city?lat=` + lat + `&lon=` + lon + `&key=f8be2bb4-74ed-4e4c-b9df-7865a95e00d9`);
+    return this.http.get(`https://api.airvisual.com/v2/nearest_city?lat=` + lat +
+    `&lon=` + lon + `&key=f8be2bb4-74ed-4e4c-b9df-7865a95e00d9`);
   }
 }
+
+
+
